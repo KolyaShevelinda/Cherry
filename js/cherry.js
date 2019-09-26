1b9ea653c3d9150ee665fc006b2d9bc8a41c7656
Nikolay shevelinda <kolyashevelinda@gmail.com>
2019-09-26 22:23:32 +0300

    comments code js file

--- js/cherry.js
+++ js/cherry.js
@@ -1,8 +1,8 @@
-function burgerFunction() {
-    var x = document.getElementById("burger");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-      } else {
-        x.style.display = "block";
-      }
-}
+// function burgerFunction() {
+//     var x = document.getElementById("burger");
+//     if (x.style.display === "block") {
+//         x.style.display = "none";
+//       } else {
+//         x.style.display = "block";
+//       }
+// }
